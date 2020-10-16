2175a9bae506497c5e2acf9193cccdab775bc10d
ishockl1 <72952621+ishockl1@users.noreply.github.com>
2020-10-16 16:51:57 -0700

    Create Exercise2.js
    Shapes were created. Need to add drag feature and confetti animation.

---
+++ Exercise2.js
@@ -0,0 +1,36 @@
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(50,175,255);
+  
+  fill(255,0,0);
+  circle(150,100,45);
+  circle(300,300,45);
+  
+  fill(0,255,0);
+  circle(300,100,45);
+  circle(450,300,45);
+  
+  fill(255,255,0);
+  circle(450,100,45);
+  circle(75,200,45);
+  
+  fill(255,150,0);
+  circle(225,200,45);
+  circle(525,200,45);
+  
+  fill(255,0,255);
+  circle(375,200,45);
+  circle(150,300,45);
+  
+}
+
+function mousePressed() {
+  
+}
+
+function mouseReleased() {
+  
+}
